47fecb5ecf3fbcd24ecddd0cc835e85caa2ddb36
Andrea Spelgatti <spelgattiandrea@ourouk.be>
2026-09-21 12:53:54 +0200

    Bump version to 0.0.5

--- src/nostalgia_launcher/core/constants.py
+++ src/nostalgia_launcher/core/constants.py
@@ -15,7 +15,7 @@ import os
 
 from .platform_support import config_dir
 
-UPDATER_VERSION = "0.0.4"
+UPDATER_VERSION = "0.0.5"
 UA = f"NostalgiaLauncher/{UPDATER_VERSION}"
 DOWNLOAD_RETRY = 5
 DOWNLOAD_TIMEOUT = 10  # seconds without any data before a transfer aborts
